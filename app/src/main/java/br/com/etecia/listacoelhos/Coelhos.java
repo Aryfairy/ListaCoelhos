670dad5c6b9277fcaed2e2f399ab4cadcf505fe5
aryfairy <ryllarygfs@gmail.com>
2024-03-19 21:55:20 -0300

    adc fts e mudando cor

--- app/src/main/java/br/com/etecia/listacoelhos/Coelhos.java
+++ app/src/main/java/br/com/etecia/listacoelhos/Coelhos.java
@@ -1,4 +1,16 @@
 package br.com.etecia.listacoelhos;
 
 public class Coelhos {
+    private String nome;
+    private int image;
+    private String descricao;
+    private String avaliacao;
+
+    public Coelhos(String nome, int image, String descricao, String avaliacao) {
+        this.nome = nome;
+        this.image = image;
+        this.descricao = descricao;
+        this.avaliacao = avaliacao;
+    }
 }
+
--- app/src/main/java/br/com/etecia/listacoelhos/ListaCoelhosAdapter.java
+++ app/src/main/java/br/com/etecia/listacoelhos/ListaCoelhosAdapter.java
@@ -1,4 +1,22 @@
 package br.com.etecia.listacoelhos;
 
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
 public class ListaCoelhosAdapter {
+    List<Coelhos> lstCoelhos;
+    Context myContext;
+
+    public ListaCoelhosAdapter(List<Coelhos> lstCoelhos, Context myContext) {
+        this.lstCoelhos = lstCoelhos;
+        this.myContext = myContext;
+    }
+
+
+
 }
+
+
